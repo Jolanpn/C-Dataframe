649a91bc32841a0837b82b9de546d4b0074ff83e
TheOnlyChou <150081420+TheOnlyChou@users.noreply.github.com>
2024-04-19 12:13:06 +0200

    Update column.h

--- column.h
+++ column.h
@@ -1,29 +1,22 @@
-//
-// Created by Jolan on 28/03/2024.
-//
-
-#ifndef PROJETC_COLUMN_H
-#define PROJETC_COLUMN_H
+#ifndef PROJETC_FONCTIONS_H
+#define PROJETC_FONCTIONS_H
 
 enum enum_type
 {
-    NULLVAL = 1 , UINT, INT,  CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
+    NULLVAL = 1 , UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
 };
 typedef enum enum_type ENUM_TYPE;
 
-
 union column_type{
-    unsigned int    uint_value;
-    signed   int    int_value;
-    char            char_value;
-    float           float_value;
-    double          double_value;
-    char*           string_value;
-    void*           struct_value;
+    unsigned int uint_value;
+    signed int int_value;
+    char char_value;
+    float float_value;
+    double double_value;
+    char* string_value;
+    void* struct_value;
 };
 typedef union column_type COL_TYPE ;
-
-
 struct column {
     char *title;
     unsigned int size; //logical size
@@ -34,22 +27,10 @@ struct column {
 };
 typedef struct column COLUMN;
 
-
-/*
-* Create a new column
-* @param1 : Column type
-* @param2 : Column title
-* @return : Pointer to the created column
-*/
 COLUMN *create_column(ENUM_TYPE type, char *title);
 
-#endif //PROJETC_COLUMN_H
+void delete_column(COLUMN **col);
 
+void convert_value(COLUMN *col, unsigned long long int i, char *str, int size);
 
-/**
-  * @brief: Insert a new value into a column
-  * @param1: Pointer to the column
-  * @param2: Pointer to the value to insert
-  * @return: 1 if the value is correctly inserted 0 otherwise
-*/
-int  insert_value(COLUMN *col, void *value);
+#endif //PROJETC_FONCTIONS_H
