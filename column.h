58e3a62cdff748e68dc2394fcc353ee97efcf6a4
Jolan Nallet--Pang <renkang@MacBook-Pro-Howe.local>
2024-04-19 13:43:55 +0200

    update column.h

--- column.h
+++ column.h
@@ -1,22 +1,25 @@
-#ifndef PROJETC_FONCTIONS_H
-#define PROJETC_FONCTIONS_H
+#ifndef PROJETC_COLUMN_H
+#define PROJETC_COLUMN_H
 
 enum enum_type
 {
-    NULLVAL = 1 , UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
+    NULLVAL = 1 , UINT, INT,  CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
 };
 typedef enum enum_type ENUM_TYPE;
 
+
 union column_type{
-    unsigned int uint_value;
-    signed int int_value;
-    char char_value;
-    float float_value;
-    double double_value;
-    char* string_value;
-    void* struct_value;
+    unsigned int    uint_value;
+    signed   int    int_value;
+    char            char_value;
+    float           float_value;
+    double          double_value;
+    char*           string_value;
+    void*           struct_value;
 };
 typedef union column_type COL_TYPE ;
+
+
 struct column {
     char *title;
     unsigned int size; //logical size
@@ -29,10 +32,12 @@ typedef struct column COLUMN;
 
 COLUMN *create_column(ENUM_TYPE type, char *title);
 
+int  insert_value(COLUMN *col, void *value);
+
 void delete_column(COLUMN **col);
 
 void convert_value(COLUMN *col, unsigned long long int i, char *str, int size);
 
 void print_col(COLUMN* col);
 
-#endif //PROJETC_FONCTIONS_H
+#endif //PROJETC_COLUMN_H
