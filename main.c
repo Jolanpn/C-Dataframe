5168546736e502b645043c65d2f263f74703600d
Jolan Nallet--Pang <renkang@MacBook-Pro-Howe.local>
2024-04-21 21:43:24 +0200

    mise à jour du main.c afin d'offrir un exemple sur l'utilisation des fonctions

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include "column.h"
+#include "cdataframe.h"
+#include "sort.h"
+#include "list.h"
+
 
 int main() {
-    /*création d'une colonne*/
-    COLUMN *mycol = create_column(CHAR, "My Column");
-    printf("%p", mycol);
+    COLUMN *mycol = create_column(CHAR, "Column 1");
+    char a[] = "A", c[] = "c";
+    insert_value(mycol, &c);
+    insert_value(mycol, NULL);
+    insert_value(mycol, &a);
+    print_col(mycol);
+    occurrence_value(mycol, &a);
+    position_value(mycol, &a);
 }
