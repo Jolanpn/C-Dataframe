08374653c2ee4f4ed40df67786707ceb5f80cfb6
TheOnlyChou <150081420+TheOnlyChou@users.noreply.github.com>
2024-05-18 17:58:17 +0200

    Update cdataframe.h

--- cdataframe.h
+++ cdataframe.h
@@ -1,12 +1,16 @@
-#ifndef PROJETC_CDATAFRAME_H
-#define PROJETC_CDATAFRAME_H
+#include "column.h"
+#ifndef CDATAFRAME_CDATAFRAME_H
+#define CDATAFRAME_CDATAFRAME_H
+// Structure maillon 
+struct mailllon
+{
+    COLUMN* col; // Pointeur structure column
+    struct mailllon * succ;
+};
+typedef struct mailllon MAILLON; // Utilisation de typedef pour créer un alias pour la structure maillon
+typedef MAILLON* CDATAFRAME;
 
-typedef LIST CDATAFRAME;
-
-/**
- * Création d'un dataframe
-*/
-CDATAFRAME *create_cdataframe(ENUM_TYPE *cdftype, int size);
-
-
-#endif //PROJETC_CDATAFRAME_H
+// Alimentation de la structure maillon
+CDATAFRAME creation_cdataframe_vide(MAILLON* premiere_colonne);
+void remplir_cdataframe_saisi(CDATAFRAME data);
+void remplir_cdataframe_dur(CDATAFRAME data);
